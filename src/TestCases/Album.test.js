4a427d1ff01ed94786846fe3a546174deb4ec123
raheman <raheman009@gmail.com>
2020-01-23 14:22:16 +0530

    testcases added for api

--- src/TestCases/Album.test.js
+++ src/TestCases/Album.test.js
@@ -15,6 +15,12 @@ describe('UserData Component Test',()=>{
     div.find('div').simulate('click');
     expect(mockcallback.mock.calls.length).toEqual(1);
   })
+  it('return function value',()=>{
+    const historyMock = { push: jest.fn() };
+    const wrapper = shallow(<Album match={{params: {id: 1}}} history={historyMock} />)  
+    expect(historyMock.push.mock.calls[0]).toEqual(undefined);
+   
+  })
   it('should display photo',()=>{
       
     const wrapper=shallow(<Album  match={{params: {id: 1}}}/>);
@@ -27,5 +33,9 @@ describe('UserData Component Test',()=>{
     wrapper.setState({albumData:['a','b','c']});
     expect(wrapper.find(userData)).toHaveLength(3);
   })
-  
+  // it('fetches successfully data from an API', async () => {
+  //   const data = [];
+  //   axios.get.mockImplementationOnce(() => Promise.resolve(data));
+  //   await expect(GlobalServices.getData('http://jsonplaceholder.typicode.com/photos?albumId=0')).resolves.toEqual(data);
+  // })
 })
--- src/TestCases/Services.test.js
+++ src/TestCases/Services.test.js
@@ -17,10 +17,12 @@ describe('Api fetch Component Test',()=>{
         axios.get.mockImplementationOnce(() => Promise.resolve(data));
         await expect(GlobalServices.getData('http://jsonplaceholder.typicode.com/photos?albumId=0')).resolves.toEqual(data);
       })
-      it('fetches error data from an API', async () => {
-        const data = [];
-        axios.get.mockImplementationOnce(() => Promise.resolve(data));
-        await expect(GlobalServices.getData('http://jsonplaceholder.typicode.com/photos?albumId=6')).resolves.toEqual(data);
+      it('fetches catch data from an API', async () => {
+        const data = 'error';
+
+        axios.get.mockRejectedValue(() => Promise.reject('error'));
+        await expect(GlobalServices.getData('http://jsonplaceholder.typicode.com/photos?albusmId=0')).rejects.toHaveLength(0);
       })
+
     
   })
